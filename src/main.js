69823ee461a74b0d6c952f6208924df4a289f85c
dmattgomez <demagome@ucsc.edu>
2020-04-19 23:32:35 -0700

    Included mod breakdown

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-console.log('hello');
+/*
+Modification Assignment Breakdown
+#################################
+Speed Increase             (10) TOTAL:10
+Random Ship Direction      (10) TOTAL:20
+Rocket Control After Fired (10) TOTAL:30
+Background Music           (10) TOTAL:40
+Display Time Remaining     (15) TOTAL:55
+Scoring Adds Time          (25) TOTAL:80
+Mouse & Click Mechanics    (25) TOTAL:105
+*/
+console.log('Background Music: A Dark Winter by Silent Protagonist');
 let config = {
     type: Phaser.CANVAS,
     width: 640,
